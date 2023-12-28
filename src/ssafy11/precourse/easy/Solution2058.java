544162f06ba09568f2d10f16b653232d4774b4e6
pokycookie <0cookieboy0@gmail.com>
2023-12-29 01:45:32 +0900

    feat: 2058.java

--- src/ssafy11/precourse/easy/Solution2058.java
+++ src/ssafy11/precourse/easy/Solution2058.java
@@ -1,4 +1,19 @@
 package ssafy11.precourse.easy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution2058 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] n = br.readLine().toCharArray();
+
+        int acc = 0;
+        for (char c : n) {
+            acc += Integer.parseInt(Character.toString(c));
+        }
+        System.out.println(acc);
+    }
 }

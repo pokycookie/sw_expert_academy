c65e6200dfe4fbfe73321ec511caa270839f6293
pokycookie <0cookieboy0@gmail.com>
2023-12-29 02:06:53 +0900

    feat: 1936.java

--- src/ssafy11/precourse/easy/Solution1936.java
+++ src/ssafy11/precourse/easy/Solution1936.java
@@ -1,4 +1,24 @@
 package ssafy11.precourse.easy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Solution1936 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(winner(a, b));
+    }
+
+    static String winner(int a, int b) {
+        if ((a + 1) % 3 == b % 3) {
+            return "B";
+        }
+        return "A";
+    }
 }

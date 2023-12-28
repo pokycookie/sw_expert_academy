6a98d4061ca39191f911e5a6ce2c60f031c9c376
pokycookie <0cookieboy0@gmail.com>
2023-12-29 01:38:57 +0900

    feat: 2063.java

--- src/ssafy11/precourse/easy/Solution2063.java
+++ src/ssafy11/precourse/easy/Solution2063.java
@@ -1,4 +1,23 @@
 package ssafy11.precourse.easy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Solution2063 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        List<Integer> sequence = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(sequence.get(n / 2));
+    }
 }

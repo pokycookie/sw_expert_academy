665e64ef4e9804d26383d946d1444cecb292afbd
pokycookie <0cookieboy0@gmail.com>
2023-12-28 19:38:22 +0900

    feat: 2072.java

--- src/ssafy11/precourse/easy/Solution2072.java
+++ src/ssafy11/precourse/easy/Solution2072.java
@@ -1,4 +1,28 @@
 package ssafy11.precourse.easy;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
 public class Solution2072 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int acc = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .filter(it -> it % 2 == 1)
+                    .reduce(0, Integer::sum);
+            bw.write(String.format("#%d %d", i + 1, acc));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }

3f8b737c970c91239ba7253063381f4ced762428
pokycookie <0cookieboy0@gmail.com>
2023-12-29 01:29:20 +0900

    feat: 2068.java

--- src/ssafy11/precourse/easy/Solution2068.java
+++ src/ssafy11/precourse/easy/Solution2068.java
@@ -1,4 +1,28 @@
 package ssafy11.precourse.easy;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
 public class Solution2068 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int max = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .max()
+                    .orElse(0);
+            bw.write(String.format("#%d %d", i + 1, max));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
 }
